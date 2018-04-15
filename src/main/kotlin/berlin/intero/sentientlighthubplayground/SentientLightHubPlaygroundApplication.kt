1a27ec0031e0bcc775b11024943a6b4c6df97adf
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-04-15 17:58:55 +0200

    Initial commit

---
+++ src/main/kotlin/berlin/intero/sentientlighthubplayground/SentientLightHubPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package berlin.intero.sentientlighthubplayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SentientLightHubPlaygroundApplication
+
+fun main(args: Array<String>) {
+    runApplication<SentientLightHubPlaygroundApplication>(*args)
+}
---
+++ src/test/kotlin/berlin/intero/sentientlighthubplayground/SentientLightHubPlaygroundApplicationTests.kt
@@ -0,0 +1,16 @@
+package berlin.intero.sentientlighthubplayground
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SentientLightHubPlaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
