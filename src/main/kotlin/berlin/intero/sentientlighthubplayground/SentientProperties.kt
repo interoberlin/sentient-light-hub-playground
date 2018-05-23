ba61c0de6e2c0994559e31fb080661af74c3bb26
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-05-23 07:44:39 +0200

    Enabled colored log messages

--- src/main/kotlin/berlin/intero/sentientlighthubplayground/SentientProperties.kt
+++ src/main/kotlin/berlin/intero/sentientlighthubplayground/SentientProperties.kt
@@ -32,5 +32,26 @@ class SentientProperties {
         // const val SENTIENT_MAPPING_DELAY = 10_000L
 
         const val VALUE_HISTORY = 50
+
+        // Colors
+
+        const val ANSI_RESET = "\u001B[0m"
+        const val ANSI_BLACK = "\u001B[30m"
+        const val ANSI_RED = "\u001B[31m"
+        const val ANSI_GREEN = "\u001B[32m"
+        const val ANSI_YELLOW = "\u001B[33m"
+        const val ANSI_BLUE = "\u001B[34m"
+        const val ANSI_PURPLE = "\u001B[35m"
+        const val ANSI_CYAN = "\u001B[36m"
+        const val ANSI_WHITE = "\u001B[37m"
+
+        val ANSI_BLACK_BACKGROUND = "\u001B[40m"
+        val ANSI_RED_BACKGROUND = "\u001B[41m"
+        val ANSI_GREEN_BACKGROUND = "\u001B[42m"
+        val ANSI_YELLOW_BACKGROUND = "\u001B[43m"
+        val ANSI_BLUE_BACKGROUND = "\u001B[44m"
+        val ANSI_PURPLE_BACKGROUND = "\u001B[45m"
+        val ANSI_CYAN_BACKGROUND = "\u001B[46m"
+        val ANSI_WHITE_BACKGROUND = "\u001B[47m"
     }
 }
